b2f0530d40974fc1f707f8c2bf5aef1648f51a1c
Kenneth Flynn <flynnkennethd@gmail.com>
2022-01-22 00:30:08 -0500

    fixed file organization issues

--- library/main.rb
+++
@@ -1,8 +0,0 @@
-require_relative('messages.rb')
-require_relative('game.rb')
-require_relative('board.rb')
-require_relative('player.rb')
-
-game = Game.new
-
-game.play_game
---
+++ main.rb
@@ -0,0 +1,8 @@
+require_relative('library/messages.rb')
+require_relative('library/game.rb')
+require_relative('library/board.rb')
+require_relative('library/player.rb')
+
+game = Game.new
+
+game.play_game
